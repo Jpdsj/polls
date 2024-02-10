7c1fab1c2b3d0839aabdcadd248a8867e9b3b129
jpdsj <junior.pires.santos@gmail.com>
2024-02-09 21:46:28 -0300

    feat: add websocket

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
+import websocket from '@fastify/websocket'
+import cookie from '@fastify/cookie'
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
-import cookie from '@fastify/cookie'
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify()
 
@@ -13,6 +15,8 @@ app.register(cookie,{
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(websocket)
+app.register(pollResults)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running');
